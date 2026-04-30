f40734f43bc21c3ce67536c558210e369c3e6a58
RezaBoushehri <reza.boshehry@gmail.com>
2026-04-30 10:10:45 +0330

    ok?

--- models/message.js
+++ models/message.js
@@ -2,25 +2,31 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true }, // Unique message ID
+    encrypt: { type: String, default: false }, // Unique message ID
     roomID: { type: String, required: true }, // The room in which the message was sent
     sender: { type: String, required: true }, // Sender's username
     quote: { type: String, default: null }, // Message content
     read: [
         {
             username: { type: String, required: true },
-            reaction: { type: String , default:null},
+            reaction: { type: String},
+            voice_heared: { type: Boolean},
             time: { type: Date, required: true },
         },
     ], 
     message: { type: String, default:null }, // Message content
     file: [
         {
-            file: { type: String, required: true },
+            file: {
+                type: String, // or another appropriate type
+                required: true // if the file field must always be included
+            },
             fileType: { type: String , required: true},
             fileName: { type: String , default:null },
         },
     ], // Optional file URL or path
     timestamp: { type: Date, default: Date.now }, // Timestamp of the message
+    edited: { type: Date, default: null }, // Timestamp of the message
 });
 
 module.exports = mongoose.models.Message || mongoose.model("Message", messageSchema);
---
+++ services/getYesterdayMessages.js
@@ -0,0 +1,75 @@
+// services/getYesterdayMessages.js
+const Room = require('../models/room');
+const Message = require('../models/message');
+const {delete_messages} = require('./del_room')
+const Log_message = require('./log')
+
+async function getMessagesUpToYesterday_file_delete(roomID=null,days_ago=1,only_files=true) {
+    // 1️⃣ ساخت تاریخ «پایان دیروز» (23:59:59.999 دیروز)
+    const endOfYesterday = new Date();
+
+    endOfYesterday.setHours(23, 59, 59, 999);   // زمان UTC؛ اگر زمان محلی می‌خواهید setHours استفاده کنید
+    endOfYesterday.setDate(endOfYesterday.getDate() - days_ago);
+    let msgs ;
+    let message='';
+    const room = await Room.findOne({roomID})
+    // 2️⃣ جستجو
+    if(only_files){
+
+        if(roomID){
+    
+            msgs = Message.find({
+                roomID,
+                timestamp: { $lte: endOfYesterday },
+                "file.0":{$exists:true}
+            }).sort({ timestamp: -1 }).cursor();   // اختیاری: جدیدترین‌ها اول
+        }else{
+            msgs = Message.find({
+                timestamp: { $lte: endOfYesterday },
+                "file.0":{$exists:true}
+            }).sort({ timestamp: -1 }).cursor();   // اختیاری: جدیدترین‌ها اول
+        }
+    }else{
+        if(roomID){
+    
+            msgs = Message.find({
+                roomID,
+                timestamp: { $lte: endOfYesterday },
+            }).sort({ timestamp: -1 }).cursor();   // اختیاری: جدیدترین‌ها اول
+        }else{
+            msgs = Message.find({
+                timestamp: { $lte: endOfYesterday },
+            }).sort({ timestamp: -1 }).cursor();   // اختیاری: جدیدترین‌ها اول
+        }
+    }
+    let msgIds= [];
+    (async () => {
+        for await (const msg of msgs) {
+            msgIds.push(msg._id)
+            // اینجا می‌توانید هر سند را به‌طور جداگانه پردازش کنید
+            
+        }
+        if(msgIds.length>0){
+            Promise.resolve(await delete_messages(msgIds)).then(res=>{
+                console.log(res);
+                message = `at ${new Date().toLocaleString('fa-IR')}
+                                \nIn: ${room?.roomName??''} #${roomID}.
+                                \n${msgIds.length} Messages deleted Successfuly before: ${endOfYesterday.toLocaleString('fa-IR')}
+                                \nDeleted_size: ${res.total_size}
+                                \nFolder_size: ${res.folder_total_size}`
+                console.log(message);
+                Log_message(message)
+            })
+
+        }else{
+            
+            message = `at ${new Date().toLocaleString('fa-IR')}\nIn: ${room?.roomName??''} #${roomID}.
+                        \nNo any message to delete with file before: ${endOfYesterday.toLocaleString('fa-IR')}`
+            console.log(message);
+            Log_message(message)
+
+        }
+    })();
+  return msgs;
+}
+module.exports = getMessagesUpToYesterday_file_delete;
